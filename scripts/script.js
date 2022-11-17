32a190ec99de11553111b3c49ad637a39e55b5bf
Hosaya Vargas <hosayavargas@gmail.com>
2022-11-17 15:40:26 -0300

    cambios importantes

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,55 @@
-let typed = new Typed (".typing",{
-    strings:["Desarrolladora web jejeje"],
-    typeSpeed:100,
-    backSpeed: 60,
-    loop: true
-})
+let typed = new Typed(".typing", {
+  strings: ["Desarrolladora web", "Frontend Developer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
+
+/* =============== Aside================*/
+const nav = document.querySelector(".nav"),
+  navList = nav.querySelectorAll("li"),
+  totalNavList = navList.length,
+  allSection = document.querySelectorAll(".section"),
+  totalSection = allSection.length;
+
+for (let i = 0; i < totalNavList; i++) {
+
+  const a = navList[i].querySelector("a");
+
+  a.addEventListener("click", function () 
+  {
+    for (let j = 0; j < totalNavList; j++) {
+      if ( navList[j].querySelector("a").classList.contains("active"))
+      {
+        console.log(navList[i].querySelector("a"))
+      }
+      navList[j].querySelector("a").classList.remove("active");
+    }
+
+    this.classList.add("active");
+    showSection(this);
+  });
+}
+function showSection(element) {
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.remove("active");
+  }
+  const target = element.getAttribute("href").split("#")[1];
+  document.querySelector("#" + target).classList.add("active");
+}
+
+const navTogglerBtn = document.querySelector(".nav-toggler"),
+  aside = document.querySelector(".aside");
+  navTogglerBtn.addEventListener("click", () =>
+  {
+    asideSectionTogglerBtn();
+  })
+  function asideSectionTogglerBtn()
+  {
+    aside.classList.toggle("open");
+    navTogglerBtn.classList.toggle("open");
+    for(let i=0; i<totalSection; i++)
+    {
+      allSection[i].classList.toggle("open");
+    }
+  }
